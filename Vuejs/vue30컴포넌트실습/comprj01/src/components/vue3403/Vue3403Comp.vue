6a2f05c81ac30e12002735a9fa3aedd313e04548
leekoreatv <leekoreatv@gmail.com>
2023-02-05 16:04:00 +0900

    정리

---
+++ Vuejs/vue30컴포넌트실습/comprj01/src/components/vue3403/Vue3403Comp.vue
@@ -0,0 +1,29 @@
+<style scoped></style>
+
+<template>
+  <div>
+    <button v-on:click="showLog">show</button>
+  </div>
+</template>
+
+<script>
+// vuex 라이브러리에서 mapActions, mapMutations, mapState, mapGetters 함를 가져옵니다.
+// import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
+
+export default {
+  /* pdtmc^2w */
+  props: [],
+  data() {
+    /* 컴포넌트 안에서 사용되는 변수 등록. 개별 변수 */
+    return {};
+  },
+  //template: ``,
+  methods: {
+    /* 이벤트 핸들러 등록 + 일반 함수 */
+    showLog(e) {
+      console.log(e.target);
+      this.$emit('showLog', 'abc', 123);
+    },
+  },
+};
+</script>
---
+++ Vuejs/vue30컴포넌트실습/comprj01/src/views/Vue3103View.vue
@@ -0,0 +1,30 @@
+<style scoped></style>
+
+<template>
+  <div id="app">
+    <h2>컴포넌트 등록</h2>
+    <Vue3103Comp v-bind:localmsg="localmsg1"></Vue3103Comp>
+    <Vue3103Comp v-bind:localmsg="localmsg2"></Vue3103Comp>
+  </div>
+</template>
+
+<script>
+// vuex 라이브러리에서 mapActions, mapMutations, mapState, mapGetters 함를 가져옵니다.
+// import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
+
+import Vue3103Comp from '../components/vue3103/Vue3103Comp.vue';
+export default {
+  /* pdtmc^2w */
+  props: [],
+  data() {
+    /* 컴포넌트 안에서 사용되는 변수 등록. 개별 변수 */
+    return {
+      localmsg1: '부모 컴포넌트엣 자식 컴포넌트로 넘겨진 메시지1',
+      localmsg2: '부모 컴포넌트엣 자식 컴포넌트로 넘겨진 메시지2',
+    };
+  },
+  components: {
+    Vue3103Comp: Vue3103Comp,
+  },
+};
+</script>
